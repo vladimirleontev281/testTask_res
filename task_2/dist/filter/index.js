1ba7eb4e34e206978503b2e772140c861241ebc4
Vladimir <vladimirleontiev.devmail@gmail.com>
2020-11-16 17:27:48 +0300

    task2_parser_and_first_attempt_of_the_class
    - решил попробовать через класс
    - набросал класс поубирал лишнее
    - мелкие правки по task3

--- task_2/dist/filter/index.js
+++ task_2/dist/filter/index.js
@@ -1,12 +1,116 @@
 "use strict";
 
-let manufacturer = document.getElementById('manufacturer');
-manufacturer.addEventListener('change', ev => {
-  let select = ev.target;
-  console.log(Array.from(select.options));
-  let selected = Array.from(select.options);
+const DEFAULT = false;
 
-    // .filter(option => option.selected)
-    // .map(option => option.value);
-  // console.log(selected);
-  });
+class task2Data {
+  constructor() {
+    this.size = '';
+    this.color = [];
+    this.manufacturer = [];
+    this.sale = '';
+
+    this.__proto__.targets = {
+      size: document.getElementsByName('sale'),
+      color: document.getElementsByClassName('color-list__input'),
+      manufacturer: Array.from(document.getElementById('manufacturer').options),
+      sale: document.getElementById('sale'),
+    };
+    this.__proto__.propList = {
+      size: {
+        multivalue: false,
+        values: new Set(['s', 'm', 'l']),
+        default: 's',
+      },
+      color: {
+        multivalue: true,
+        values: new Set(['1', '2', '3', '4', '5']),
+        default: null,
+      },
+      manufacturer: {
+        multivalue: true,
+        values: new Set(['aaa', 'b&c',  'ddd', 'eee']),
+        default: 'aaa',
+      },
+      sale: {
+        multivalue: false,
+        values: new Set(['0', '1']),
+        default: null,
+      },
+    }
+  }
+
+  isValidValue(propName, value) {
+    return this.propList.hasOwnProperty(propName) ? this.propList[propName].values.has(value) : false;
+  }
+
+  // получает значения DOM-элементов и перезаписывает ими данные объекта
+  getValuesInDOM() {
+    
+  }
+
+  // устагавливает значения DOM-элементов
+  // в соответствии с текущими значениями объекта
+  setValuesInDOM() {
+    
+  }
+
+  // устанавливает значения внутреннего объекта в соответствии с полученным
+  setObjValues(obj) {
+    for (const key in obj) {
+      
+    }
+  }
+}
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.log(getURLDetails());
+  
+});
+
+function getURLDetails() {
+  let output = {};
+  let details = (location.search) ? 
+    location.search.slice(1, location.search.length).split('&').map(item => item.split('=')) : null;
+
+  if (details) {
+    for (let i = 0; i < details.length; i++) {
+      if (output.hasOwnProperty(details[i][0])) {
+        output[details[i][0]].push(details[i][1]);
+      } else {
+        output[details[i][0]] = [details[i][1]];
+      }
+    }
+    for (const key in output) {
+      output[key] = (output[key].length > 1) ? output[key] : output[key][0];
+    }
+    return output;
+  } else {
+    return null;
+  }
+}
+
+
+
+
+
+
+
+
+// function objIsEmpty(obj) {
+//   for (let key in obj) {
+//     return false;
+//   }
+//   return true;
+// }
+
+// let manufacturer = document.getElementById('manufacturer');
+// manufacturer.addEventListener('change', ev => {
+//   let select = ev.target;
+//   console.log(Array.from(select.options));
+//   let selected = Array.from(select.options)
+//     .filter(option => option.selected)
+//     .map(option => option.value);
+//   console.log(selected);
+// });
+
